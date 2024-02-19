4ff9f13590ef55b37dc4b3343162d8583d71d84e
Ianel <ianell.s118@gmail.com>
2024-02-19 10:19:56 +0300

    add two levels on astronomy topic

--- src/datas/questions.js
+++ src/datas/questions.js
@@ -1,30 +1,221 @@
 export const quizData = {
     Astronomy: [
         {
-            id: 0,
-            question: "Who is the first US President star ?",
-            answers: [
+            "Level 1": [
                 {
-                    label: "Abraham Lincoln",
-                    isCorrect: false,
+                    id: 0,
+                    question:
+                        "Which is the biggest planet of our solar system ?",
+                    answers: [
+                        {
+                            label: "Mars",
+                            isCorrect: false,
+                        },
+                        {
+                            label: "Jupiter",
+                            isCorrect: true,
+                        },
+                    ],
                 },
                 {
-                    label: "George Washington",
-                    isCorrect: true,
+                    id: 1,
+                    question:
+                        "What is the name of the biggest moon of our solar system ?",
+                    answers: [
+                        {
+                            label: "Ganymede",
+                            isCorrect: true,
+                        },
+                        {
+                            label: "Europa",
+                            isCorrect: false,
+                        },
+                    ],
+                },
+                {
+                    id: 2,
+                    question: "Who was the first human to walk on the moon ?",
+                    answers: [
+                        {
+                            label: "Neil Armstrong",
+                            isCorrect: true,
+                        },
+                        {
+                            label: "Buzz Aldrin",
+                            isCorrect: false,
+                        },
+                    ],
+                },
+                {
+                    id: 3,
+                    question: "What does NASA stand for ?",
+                    answers: [
+                        {
+                            label: "National Aerospace and Science Agency",
+                            isCorrect: false,
+                        },
+                        {
+                            label: "National Aeronautics and Space Administration",
+                            isCorrect: true,
+                        },
+                    ],
+                },
+                {
+                    id: 4,
+                    question: "Where is the VLT located ?",
+                    answers: [
+                        {
+                            label: "Chile",
+                            isCorrect: true,
+                        },
+                        {
+                            label: "Arizona",
+                            isCorrect: false,
+                        },
+                    ],
                 },
             ],
-        },
-        {
-            id: 1,
-            question: "Who is Elon Musk ?",
-            answers: [
+            "Level 2": [
                 {
-                    label: "A rich genius",
-                    isCorrect: true,
+                    id: 0,
+                    question: "What is the most powerful rocket ever built ?",
+                    answers: [
+                        {
+                            label: "Space Launch System",
+                            isCorrect: true,
+                        },
+                        {
+                            label: "Saturn V",
+                            isCorrect: false,
+                        },
+                    ],
                 },
                 {
-                    label: "A popular singer",
-                    isCorrect: false,
+                    id: 1,
+                    question: "Where is Baikonour located ?",
+                    answers: [
+                        {
+                            label: "Russia",
+                            isCorrect: false,
+                        },
+                        {
+                            label: "Kazhakstan",
+                            isCorrect: true,
+                        },
+                    ],
+                },
+                {
+                    id: 2,
+                    question: "What rockets do ESA operate ?",
+                    answers: [
+                        {
+                            label: "Ariane VI, Vega, Ariane V",
+                            isCorrect: true,
+                        },
+                        {
+                            label: "Atlas V, Delta Heavy, Antares",
+                            isCorrect: false,
+                        },
+                    ],
+                },
+                {
+                    id: 3,
+                    question: "Which president started the Appollo program ?",
+                    answers: [
+                        {
+                            label: "Richard Nixon",
+                            isCorrect: false,
+                        },
+                        {
+                            label: "John Fitzerald Kennedy",
+                            isCorrect: true,
+                        },
+                    ],
+                },
+                {
+                    id: 4,
+                    question: "How many planets do our solar system have ?",
+                    answers: [
+                        {
+                            label: "8",
+                            isCorrect: true,
+                        },
+                        {
+                            label: "9",
+                            isCorrect: false,
+                        },
+                    ],
+                },
+                {
+                    id: 5,
+                    question:
+                        "Which spacecraft is the farthest object ever send by human into space ?",
+                    answers: [
+                        {
+                            label: "Voyager 1",
+                            isCorrect: true,
+                        },
+                        {
+                            label: "New Horizon",
+                            isCorrect: false,
+                        },
+                    ],
+                },
+                {
+                    id: 6,
+                    question: "Who is the founder of SpaceX ?",
+                    answers: [
+                        {
+                            label: "Elon Musk",
+                            isCorrect: true,
+                        },
+                        {
+                            label: "Jeff Bezos",
+                            isCorrect: false,
+                        },
+                    ],
+                },
+                {
+                    id: 7,
+                    question: "What does STS stand for ?",
+                    answers: [
+                        {
+                            label: "Space Training System",
+                            isCorrect: false,
+                        },
+                        {
+                            label: "Space Transportation System",
+                            isCorrect: true,
+                        },
+                    ],
+                },
+                {
+                    id: 8,
+                    question: "Which nation is the maker of HTV ?",
+                    answers: [
+                        {
+                            label: "Japan",
+                            isCorrect: true,
+                        },
+                        {
+                            label: "China",
+                            isCorrect: false,
+                        },
+                    ],
+                },
+                {
+                    id: 9,
+                    question: "What does ESO stand for ?",
+                    answers: [
+                        {
+                            label: "European Southern Observatory",
+                            isCorrect: true,
+                        },
+                        {
+                            label: "European Space Organization",
+                            isCorrect: false,
+                        },
+                    ],
                 },
             ],
         },
